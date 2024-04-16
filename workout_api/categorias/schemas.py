9f7ab90362fd093e45d91bccab3269126753b850
Joel de Oliveira <coffee.program.joel@gmail.com>
2024-04-16 19:37:21 -0300

    Até: Criação das rotas de Categoria

--- workout_api/categorias/schemas.py
+++ workout_api/categorias/schemas.py
@@ -1,10 +1,14 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import Field, UUID4
 
 from workout_api.contrib.schemas import BaseSchema
 
 
-class Categoria(BaseSchema):
+class CategoriaIn(BaseSchema):
     nome: Annotated[str, Field(description="Nome da categoria",
                                example="Scale", max_length=10)]
+
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(descripiton="Identificador da categoria")]
